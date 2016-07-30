b7cccfcd00a7e766ea44898e46cb24ada5c05893
bcasadei <bill@billcasadei.com>
2016-07-30 15:22:29 -0600

    Added pascals_triangle

---
+++ pascals_triangle/pascals_triangle.rb
@@ -0,0 +1,14 @@
+class Triangle
+
+  def initialize(rows)
+    @rows = []
+  end
+
+  def rows
+    #[[], [], []]
+    # for each row look at the last row (current row index -1) and
+    # the number on either side of the current indev (current 
+    # number = previous rows (current index -1 + current index +1 )
+  end
+
+end
