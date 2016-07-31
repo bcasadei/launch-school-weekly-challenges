1705a2d8e44ec54d690de38cb19003cab033796b
bcasadei <bill@billcasadei.com>
2016-07-30 21:43:40 -0600

    1st draft pascals_triangle

--- pascals_triangle/pascals_triangle.rb
+++ pascals_triangle/pascals_triangle.rb
@@ -1,14 +1,37 @@
+require 'pry'
+
 class Triangle
 
-  def initialize(rows)
-    @rows = []
+  def initialize(number)
+    @number = number
+    @triangle = []
   end
 
   def rows
-    #[[], [], []]
-    # for each row look at the last row (current row index -1) and
-    # the number on either side of the current indev (current 
-    # number = previous rows (current index -1 + current index +1 )
+    rows = @number - 1
+
+    0.upto rows do |row|
+      if row == 0
+        @triangle << [1]
+      elsif row == 1
+        @triangle << [1] + [1]
+      else
+        current = 1
+
+        @triangle << [1]
+          until current >= row
+            p l_num = @triangle[row - 1][current - 1]
+            p r_num = @triangle[row - 1][current]
+            number = l_num + r_num
+            @triangle[row] << number
+            current += 1
+          end
+
+        @triangle[row] << 1
+      end
+    end
+
+    @triangle
   end
 
 end
